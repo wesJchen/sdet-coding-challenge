8ce8268740a3b1f08ae99f37ab02ca97a9a48c46
Wesley Chen <wesleychen@wesMacm3.T-mobile.com>
2023-12-03 10:47:23 -0600

    Update locators and solution selection logic

--- page_objects/elements.py
+++ page_objects/elements.py
@@ -1,12 +1,23 @@
-from selenium.webdriver.common.by import By
-
 class PageElements:
 
     base_url = "http://sdetchallenge.fetch.com/"
 
+    # Buttons
     reset_button_xpath = '/html/body/div/div/div[1]/div[4]/button[1]'    
     weigh_button_xpath = '//*[@id="weigh"]'
 
-    gold_options = '//*[@id="root"]/div/div[2]'
+    # Gold options
+    gold_options_xpath = '//*[@id="root"]/div/div[2]'
+    gold_options_child_xpath = './*'
 
-    results_xpath = '//*[@id="root"]/div/div[1]/div[5]/ol'
+    # Basket locators
+    left_basket_id = 'left_{}'
+    right_basket_id = 'right_{}'
+
+    # Solution Locators
+    results_xpath = '//*[@id="reset"]'
+    results_reset_text = '?'
+
+    fake_bar_solution_xpath = '//*[@id="coin_{}"]'
+    incorrect_alert_text ='Oops! Try Again!' 
+    correct_alert_text = 'Yay! You find it!'
