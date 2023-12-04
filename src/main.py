2aa73d2d23fab924faa62fdf1e37a7102fb6611c
Wesley Chen <wesleychen@wesjchenmacm3.T-mobile.com>
2023-12-03 23:49:07 -0600

    remove unused import

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import time
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.alert import Alert
 from selenium.common.exceptions import NoAlertPresentException
